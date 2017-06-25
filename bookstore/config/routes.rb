7b08831326574e6f8301b711396ff529178a04c6
ravenvn <ravenvn@yahoo.com.vn>
2017-06-25 18:56:54 +0700

    bookstore

---
+++ bookstore/config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'about' => 'about#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'books#index'
+
+  resources :books
+  resources :authors
+  resources :categories
+  resources :publishers
+end
